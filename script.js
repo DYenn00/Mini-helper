9af7b05fedef1d15f971114e314d566bf5eb1973
DYenn00 <55598370+DYenn00@users.noreply.github.com>
2025-10-25 14:33:11 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-//Logic for random messages and activity
+//logic for random messages and activity
+const popup = document.getElementById("popup");
+const buddy = document.getElementById("helper");
+
+const messages = [
+  "Hey there! Need help?",
+  "Uhh..did you remember to save your work 💾",
+  "Just a reminder..You’re doing great today!",
+  "Remember to take short breaks ☕",
+  "Push your code to GitHub regularly 🚀",
+  "Keep your code clean and commented 🧹"
+];
+
+buddy.addEventListener("click", () => {
+  popup.textContent = messages[Math.floor(Math.random() * messages.length)];
+  popup.classList.add("show");
+
+  // go idle after a short delay
+  setTimeout(() => {
+    popup.classList.remove("show");
+  }, 4000);
+});
